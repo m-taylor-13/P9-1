57d94fde329ae54355799a58ebe5d0bc2dc42824
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-30 12:18:08 -0400

    Prints the random integers

--- Project25/FileName.cpp
+++ Project25/FileName.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main(void) {
+	srand(time(0));
+
+	int numbers[10];
+
+	for (int i = 0; i < 10; i++) {
+		numbers[i] = rand() % 10000;
+
+		//ensures all numbers are 4 digit
+		if (numbers[i] < 1000) {
+			numbers[i] += 1000;
+		}
+	}
+
+
+	cout << "The random integers: ";
+	for (int i = 0; i < 10; i++) {
+		cout << numbers[i] << "  ";
+	}
+	cout << endl;
+
 
 }
